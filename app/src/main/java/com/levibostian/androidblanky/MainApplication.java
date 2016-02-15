acfddbd39a06e849a159c84bc2aa6561942f3640
Levi Bostian <levi.bostian@gmail.com>
2016-02-14 21:57:03 -0600

    Install LeakCanery library to project.

--- app/src/main/java/com/levibostian/androidblanky/MainApplication.java
+++ app/src/main/java/com/levibostian/androidblanky/MainApplication.java
@@ -1,7 +1,14 @@
 package com.levibostian.androidblanky;
 
 import android.app.Application;
+import com.squareup.leakcanary.LeakCanary;
 
 public class MainApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        LeakCanary.install(this);
+    }
 }
