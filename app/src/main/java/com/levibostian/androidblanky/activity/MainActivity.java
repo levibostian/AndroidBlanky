d2222a0a0bc2edafc5455a5651a5fe73c15aecca
Levi Bostian <levi.bostian@gmail.com>
2016-02-14 19:00:20 -0600

    Use Toolbar as the app actionbar.

--- app/src/main/java/com/levibostian/androidblanky/activity/MainActivity.java
+++ app/src/main/java/com/levibostian/androidblanky/activity/MainActivity.java
@@ -1,18 +1,29 @@
-package com.levibostian.androidblanky;
+package com.levibostian.androidblanky.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import com.levibostian.androidblanky.R;
 import com.levibostian.androidblanky.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        setupView();
 
         startupFragment();
+    }
+
+    private void setupView() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
     }
 
     private void startupFragment() {
